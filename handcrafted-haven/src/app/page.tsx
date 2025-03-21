376442c94ee497f7538c900339f143230ef858af
Ezequiel Rindello <rindelloezequiel@gmail.com>
2025-03-20 22:25:09 -0300

    Add hero image component and styles to landing page

--- handcrafted-haven/src/app/page.tsx
+++ handcrafted-haven/src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import HeroImg from "./ui/landing-page/hero-img";
 
 export default function Home() {
   return (
     <div>
       landing page
+      <HeroImg />
     </div>
   );
 }
