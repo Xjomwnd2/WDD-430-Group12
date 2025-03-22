73a499eae1b84ace8b45bb958ca0d683e2311e65
Sariah <sariahwandler@gmail.com>
2025-03-22 12:28:11 -0600

    product-type section of landing page

--- handcrafted-haven/src/app/page.tsx
+++ handcrafted-haven/src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import HeroImg from "./ui/landing-page/hero-img";
+import ProductTypesWrapper from "./ui/landing-page/product-type";
 
 export default function Home() {
   return (
     <div>
       {/* landing page */}
       <HeroImg />
+      <ProductTypesWrapper />
     </div>
   );
 }
