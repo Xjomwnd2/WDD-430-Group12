a24ea8e259a830f3e8ee272c3d4f037cc585439f
Ezequiel Rindello <rindelloezequiel@gmail.com>
2025-03-31 21:31:29 -0300

    refactor: update seller page to comment out non-functional ProductsWrapper and improve API URL handling

--- handcrafted-haven/src/app/sellers/[id]/page.tsx
+++ handcrafted-haven/src/app/sellers/[id]/page.tsx
@@ -1,17 +1,16 @@
 import ContactForm from "@/app/ui/sellers/contact-form";
-import ProductsWrapper from "@/app/ui/sellers/products";
+// import ProductsWrapper from "@/app/ui/sellers/products";
 import SellerInfo from "@/app/ui/sellers/seller-info";
 
-export default async function Page(props: {params: Promise<{id: string}>}){
-    const params = await props.params;
-    const id = params.id;
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+  const params = await props.params;
+  const id = params.id;
 
-    return(
-        <div>
-        <SellerInfo user_id={id} />
-        <ProductsWrapper user_id={id} />
-        <ContactForm />
-        </div>
-    )
-    
+  return (
+    <div>
+      <SellerInfo user_id={id} />
+      {/* <ProductsWrapper user_id={id} /> */} {/* this is not working */}
+      <ContactForm />
+    </div>
+  );
 }
