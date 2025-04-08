bd434414899c57f3f342c69a4ea892db4961a8a3
Ezequiel Rindello <rindelloezequiel@gmail.com>
2025-04-07 21:56:24 -0300

    implement user authentication with login and registration; add bcrypt for password hashing and create respective API routes

--- handcrafted-haven/src/app/account/page.tsx
+++ handcrafted-haven/src/app/account/page.tsx
@@ -1,3 +1,19 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import Loading from "../ui/products-page/loading";
+
 export default function Page() {
-  return <p>Account Page</p>;
+  const router = useRouter();
+
+  useEffect(() => {
+    const isLogged = localStorage.getItem("isLogged");
+
+    if (!isLogged) {
+      router.push("/login");
+    }
+  }, [router]);
+
+  return <Loading />;
 }
