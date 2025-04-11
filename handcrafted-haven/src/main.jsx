4e108529171e2bd8196d612ac367643c9034a55b
YourGitHubUsername <jmwaura@byupathway.edu>
2025-04-11 11:54:57 +0300

    COMMIT

---
+++ handcrafted-haven/src/main.jsx
@@ -0,0 +1,14 @@
+// src/main.jsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello Group 12! 🎉</h1>
+      <p>This is your Vite + React setup.</p>
+    </div>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
